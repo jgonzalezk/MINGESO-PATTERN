96d11fcf25b782c73f6747f33fee176da40cabac
jgonzalezk <jorge.gonzalez.q@usach.cl>
2022-04-12 01:08:45 -0400

    Adapter Pattern Problem

---
+++ Adapter/TestMain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Problema: Un sistema estadounidense requiere
+// grados Fahrenheit para ser utilizados pero
+// donde se desea utilizar el sistema la temperatura 
+// se mide en grados Celcius.
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese");
+    }
+}
